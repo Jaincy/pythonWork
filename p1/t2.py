49fe00dbdd0cab12d59442844455645bf1e3fcf7
duzhixin <609447971@qq.com>
2017-06-14 10:45:43 +0800

    BASE64解码

--- p1/t2.py
+++ p1/t2.py
@@ -1,7 +1,11 @@
-# encoding=utf-8
+import base64
+
 
 class c1:
-    bb = "bb"
+    def __init__(self):
+        pass
+
+    bb = "zz"
 
     def test1(self):
         print "test1"
@@ -20,4 +24,8 @@ cc.test1()
 cc = c1()
 cc.test1()
 
-
+b64data = open('base64data.txt').read()
+img_data = b64data.decode('base64')
+fw = open('img.jpg', 'wb')
+fw.write(img_data)
+fw.close()
